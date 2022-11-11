66529a0cd98274337c6fe800c9998f5f35d149ae
SimonZone <sb.simonzone@gmail.com>
2022-11-11 16:08:33 +0100

    Removed a mistake

--- Obligatorisk Database Opgave/DBClient.cs
+++ Obligatorisk Database Opgave/DBClient.cs
@@ -6,7 +6,7 @@ internal class DBClient
     public void Start(string queryString)
     {
         // The connectionString is connected to my azure database with the same data as what would be in the txt files
-        string connectionString = "Server=tcp:simon-sql-server-1.database.windows.net,1433;Initial Catalog=Simon_SQL_Database_1;Persist Security Info=False;User ID=SimonSAdmin;Password=SimonAzurePW1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=HotelDBRE;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
 
         using SqlConnection connection = new(connectionString);
